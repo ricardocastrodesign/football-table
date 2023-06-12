2743f55ce817fcf069029a9fc9ca926bf5ca2ed5
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-06-12 23:09:38 +0100

    add football table component

---
+++ src/components/FootballTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Premier League Standings</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueFootballTable',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
