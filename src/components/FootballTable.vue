7aa27a292129f4718e16cff7efac54fabdf448bd
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-06-13 00:06:08 +0100

    add headers

--- src/components/FootballTable.vue
+++ src/components/FootballTable.vue
@@ -4,7 +4,21 @@
     <template v-slot:default>
       <thead>
         <tr>
+        <!-- Remove Text-->
+          <th class="text-left">Position</th>
+          <th class="text-left">Logo</th>
           <th class="text-left">Name</th>
+        <!-- End Remove Text -->
+
+          <th class="text-left">Form</th>
+          <th class="text-left">GP</th>
+          <th class="text-left">W</th>
+          <th class="text-left">D</th>
+          <th class="text-left">L</th>
+          <th class="text-left">GF</th>
+          <th class="text-left">GA</th>
+          <th class="text-left">GD</th>
+          <th class="text-left">Pts</th>
         </tr>
       </thead>
       <tbody>
