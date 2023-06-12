a5ae6d07eed08348f95691d67865ebabb65d942d
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-06-12 23:17:39 +0100

    add get request

--- src/components/FootballTable.vue
+++ src/components/FootballTable.vue
@@ -1,29 +1,36 @@
 <template>
-    <div>
-        <h1>Premier League Standings</h1>
-    </div>
+  <div>
+    <h1>Premier League Standings</h1>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: 'VueFootballTable',
+  name: "VueFootballTable",
 
-    data() {
-        return {
-            
-        };
-    },
+  data() {
+    return {};
+  },
 
-    mounted() {
-        
-    },
+  mounted() {
+    this.getStandings();
+  },
 
-    methods: {
-        
+  methods: {
+    getStandings() {
+      axios
+        .get(
+          "https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2020-2021"
+        )
+        .then((response) => {
+          console.log(response);
+        });
     },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
