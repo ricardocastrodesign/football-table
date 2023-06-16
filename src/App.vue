390ee0bd8378a3c671dcc325f6f4dbe74ecbc56d
Ricardo Castro <ricardocastrodesign@gmail.com>
2023-06-16 01:04:52 +0100

    Add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        Premier League Table       
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-switch
+        v-model="darkTheme"
+        label="Light/Dark Mode"
+        @change="toggleTheme"
+      ></v-switch>
     </v-app-bar>
 
     <v-main>
-      <FootballTable/>
+      <FootballTable />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import FootballTable from './components/FootballTable';
+import FootballTable from "./components/FootballTable";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    FootballTable
+    FootballTable,
   },
 
   data: () => ({
-    //
+    darkTheme: false,
   }),
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = this.darkTheme;
+    },
+  },
 };
 </script>
